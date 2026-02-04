779d5778e788355854506c33d9d48f07d8b351bf
hoer369 <opec111@qq.com>
2026-02-04 13:36:03 +0800

    chore: move build files to docs folder

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/yk3dshow/', // 必须与你的仓库名一致，前后都要有斜杠
   plugins: [react()],
   server: {
     host: true, // 允许外部访问
